2cd0ec73801801908fa98c3c2045ecf4af2e73c9
KalashVovan <72902666+KalashVovan@users.noreply.github.com>
2022-10-12 22:29:01 +0500

    1st

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// libraries
+import React from 'react';
+// components
+import Header from './Header.js';
+// styles
+import './App.css';
+
+// const App = () => {}
+function App() {
+  return (
+    <div>
+      <div className="App">
+        <Header/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="Header_Background">
+            <div className='Container'>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Header;
