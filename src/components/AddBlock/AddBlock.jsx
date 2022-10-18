4e187caaccf84eb6de47115985edf63dd168ca47
KalashVovan <72902666+KalashVovan@users.noreply.github.com>
2022-10-18 21:58:10 +0500

    Examples

--- src/components/AddBlock/AddBlock.jsx
+++ src/components/AddBlock/AddBlock.jsx
@@ -3,7 +3,9 @@ import s from './AddBlock.module.css'
 const AddBlock = () => {
   return (
     <div id='AddBlock' className={s.background}>
-      
+      <div className={s.example1}>Num1</div>
+      <div className={s.example1}>Num1</div>
+      <div className={s.example1}>Num1</div>
     </div>
   )
 }
