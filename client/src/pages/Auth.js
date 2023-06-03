12824295b8e6dbb17081b975757f1a4eb0e7f85e
Calisthetic <vovakalash04@mail.ru>
2023-06-03 20:04:56 +0500

    UI added

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import s from "./Pages.module.css"
+import s from "./Auth.module.css";
 
 
 
@@ -8,25 +8,45 @@ const Auth = () => {
   const textPasswordRef = useRef()
 
   async function Entry() {
-    let user = await fetch('/api/userAuth?name=' + textLoginRef.current.value 
-      + "&login=" + textLoginRef.current.value + "&password=" + textPasswordRef.current.value, {
-      method: 'GET',
-    })
-    if (user.status === 200) {
-      user = await user.json()
-      localStorage.setItem("userID", user.id)
-      window.location.href = "/projects";
+    if (textPasswordRef.current.value.length > 2 && textLoginRef.current.value.length > 2) {
+      let user = await fetch('/api/userAuth?name=' + textLoginRef.current.value 
+        + "&login=" + textLoginRef.current.value + "&password=" + textPasswordRef.current.value, {
+        method: 'GET',
+      })
+      if (user.status === 200) {
+        user = await user.json()
+        localStorage.setItem("userID", user.id)
+        window.location.href = "/projects";
+      } else {
+        alert("Пользователь не найден")
+      }
     } else {
-      alert("Пользователь не найден")
+      textLoginRef.current.style.border = "2px solid red";
+      textLoginRef.current.style.border = "2px solid red";
     }
   }
 
   return (
-    <div className={s.background}>
+    <div className={s.background_auth}>
       <div className={s.field_back}>
-        <input type="text" defaultValue="user1" ref={textLoginRef} placeholder="login or name" className={s.input}></input>
-        <input type="text" defaultValue="pass1" ref={textPasswordRef} placeholder="password" className={s.input}></input>
-        <div onClick={Entry} className={s.button}>Entry</div>
+        <div className={s.title}>Welcome back!</div>
+        <input
+          type="text"
+          defaultValue="user1"
+          ref={textLoginRef}
+          placeholder="login or name"
+          className={s.input_text}
+        ></input>
+        <input
+          type="text"
+          defaultValue="pass1"
+          ref={textPasswordRef}
+          placeholder="password"
+          className={s.input_text}
+        ></input>
+        <div onClick={Entry} className={s.button}>
+          Entry
+        </div>
       </div>
     </div>
   )
--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -1,7 +1,14 @@
-import { useEffect, useState } from "react";
-import s from "./Pages.module.css"
+import { useEffect, useState, useRef } from "react";
+import s from "./Projects.module.css";
 
 const Projects = () => {
+  const [addOpen, setAddOpen] = useState(false);
+
+  const addProjectButtonRef = useRef();
+  const addProjectOpenButtonRef = useRef();
+  const addProjectFieldRef = useRef();
+  const addProjectNameRef = useRef();
+
   const [ProjectsData, setProjectsData] = useState([{}])
   useEffect(() => {
     fetch("/api/project/" + localStorage.getItem("userID"), {method: 'GET'}).then(
@@ -18,18 +25,89 @@ const Projects = () => {
     window.location.href = "/main";
   }
 
+  function addProjectButtonClick() {
+    if (!addOpen) {
+      addProjectFieldRef.current.style.width = "214px";
+      addProjectFieldRef.current.style.marginLeft = "30px";
+      addProjectOpenButtonRef.current.textContent = "−";
+      addProjectOpenButtonRef.current.style.transform = "rotate(180deg)";
+      addProjectOpenButtonRef.current.style.color = "#ff0000";
+      setAddOpen(!addOpen);
+    } else {
+      addProjectFieldRef.current.style.width = "0px";
+      addProjectFieldRef.current.style.marginLeft = "0px";
+      addProjectOpenButtonRef.current.textContent = "+";
+      addProjectOpenButtonRef.current.style.transform = "rotate(0deg)";
+      addProjectOpenButtonRef.current.style.color = "#00ff00";
+      setAddOpen(!addOpen);
+    }
+  }
+  function addProject() {
+    if (addProjectNameRef.current.value.length > 2) {
+      addProjectNameRef.current.value = "";
+      addProjectButtonClick();
+    } else {
+      addProjectButtonRef.current.style.color = "#ff0000";
+      setTimeout(() => {
+        addProjectButtonRef.current.style.color = "lime";
+      }, 500);
+    }
+  }
+
   return (
-    <div className={s.background}>
-      <div className={s.cages}>
-        {(typeof ProjectsData[0] == "undefined") ? (
-          <p>Loading...</p>
-          ) : (
-            ProjectsData.map((item, index) => (
-              <div key={index} id={item.id} data-id={item.id} className={s.cage} onClick={selectProject}>{item.name}</div>
-            ))
-        )}
+    <div className={s.background_projects}>
+    <div className={s.projects_field}>
+      <div className={s.cages_back}>
+        <div title="Click to edit your project" className={s.cage_title}>
+          ВАШИ ПРОЕКТЫ
+        </div>
+        <div className={s.cages}>
+          {(typeof ProjectsData[0] == "undefined") ? (
+            <p>Loading...</p>
+            ) : (
+              ProjectsData.map((item, index) => (
+                <div className={s.cage_back}>
+                  <div key={index} id={item.id} data-id={item.id} className={s.cage} onClick={selectProject}>
+                    <div className={s.cage_name}>{item.name}</div>
+                    <div className={s.cage_blocks_count}>{item.blocks_count}</div>
+                  </div>
+                </div>
+              ))
+          )}
+          <div className={s.cage_back}>
+            <div className={s.cage} onClick={selectProject}>
+              <div className={s.cage_name}>Project5</div>
+              <div className={s.cage_blocks_count}>0</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div ref={addProjectFieldRef} className={s.add_project}>
+        <div className={s.add_title}>НОВЫЙ ПРОЕКТ</div>
+        <input
+          maxLength="32"
+          placeholder="название проекта"
+          type="text"
+          ref={addProjectNameRef}
+          className={s.add_input_name}
+        ></input>
+        <div
+          className={s.add_button}
+          ref={addProjectButtonRef}
+          onClick={addProject}
+        >
+          Создать
+        </div>
       </div>
     </div>
+    <div
+      className={s.add_open_button}
+      ref={addProjectOpenButtonRef}
+      onClick={addProjectButtonClick}
+    >
+      +
+    </div>
+  </div>
   )   
 }
 export default Projects
