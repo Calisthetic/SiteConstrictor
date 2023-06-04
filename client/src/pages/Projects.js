08146989f9c0fe2697ef0a88aaa702b04e160957
Calisthetic <vovakalash04@mail.ru>
2023-06-04 07:50:00 +0500

    add projects

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -6,9 +6,11 @@ const Projects = () => {
 
   const addProjectButtonRef = useRef();
   const addProjectOpenButtonRef = useRef();
+  const addProjectOpenButtonTextRef = useRef();
   const addProjectFieldRef = useRef();
   const addProjectNameRef = useRef();
 
+  const [isUpdate, setIsUpdate] = useState(false)
   const [ProjectsData, setProjectsData] = useState([{}])
   useEffect(() => {
     fetch("/api/project/" + localStorage.getItem("userID"), {method: 'GET'}).then(
@@ -18,10 +20,10 @@ const Projects = () => {
         setProjectsData(data)
       }
     )
-  }, []);
+  }, [isUpdate]);
 
   function selectProject(event) {
-    localStorage.setItem("projectID", event.target.id)
+    localStorage.setItem("projectID", event.target.dataset.id)
     window.location.href = "/main";
   }
 
@@ -29,23 +31,39 @@ const Projects = () => {
     if (!addOpen) {
       addProjectFieldRef.current.style.width = "214px";
       addProjectFieldRef.current.style.marginLeft = "30px";
-      addProjectOpenButtonRef.current.textContent = "−";
+      addProjectOpenButtonTextRef.current.textContent = "−";
       addProjectOpenButtonRef.current.style.transform = "rotate(180deg)";
       addProjectOpenButtonRef.current.style.color = "#ff0000";
       setAddOpen(!addOpen);
     } else {
       addProjectFieldRef.current.style.width = "0px";
       addProjectFieldRef.current.style.marginLeft = "0px";
-      addProjectOpenButtonRef.current.textContent = "+";
+      addProjectOpenButtonTextRef.current.textContent = "+";
       addProjectOpenButtonRef.current.style.transform = "rotate(0deg)";
       addProjectOpenButtonRef.current.style.color = "#00ff00";
       setAddOpen(!addOpen);
     }
   }
-  function addProject() {
+  async function addProject() {
     if (addProjectNameRef.current.value.length > 2) {
-      addProjectNameRef.current.value = "";
-      addProjectButtonClick();
+      let block = {
+        user_id: localStorage.getItem("userID"),
+        name: addProjectNameRef.current.value,
+      }
+      let response = await fetch('/api/project', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(block)
+      });
+      if (response.status === 200) {
+        setIsUpdate(!isUpdate)
+        addProjectNameRef.current.value = "";
+        addProjectButtonClick();
+      } else {
+        alert("Не удалось создать новый проект...")
+      }
     } else {
       addProjectButtonRef.current.style.color = "#ff0000";
       setTimeout(() => {
@@ -66,20 +84,18 @@ const Projects = () => {
             <p>Loading...</p>
             ) : (
               ProjectsData.map((item, index) => (
-                <div className={s.cage_back}>
-                  <div key={index} id={item.id} data-id={item.id} className={s.cage} onClick={selectProject}>
-                    <div className={s.cage_name}>{item.name}</div>
-                    <div className={s.cage_blocks_count}>{item.blocks_count}</div>
+                <div className={s.cage_back} key={index}>
+                  <div id={item.id} data-id={item.id} className={s.cage} onClick={selectProject}>
+                    <div data-id={item.id} className={s.cage_name}>{item.name}</div>
+                    <div 
+                      data-id={item.id}
+                      className={s.cage_blocks_count} 
+                      title="Количество элементов на странице">{item.blocks_count}
+                    </div>
                   </div>
                 </div>
               ))
           )}
-          <div className={s.cage_back}>
-            <div className={s.cage} onClick={selectProject}>
-              <div className={s.cage_name}>Project5</div>
-              <div className={s.cage_blocks_count}>0</div>
-            </div>
-          </div>
         </div>
       </div>
       <div ref={addProjectFieldRef} className={s.add_project}>
@@ -105,7 +121,7 @@ const Projects = () => {
       ref={addProjectOpenButtonRef}
       onClick={addProjectButtonClick}
     >
-      +
+      <div ref={addProjectOpenButtonTextRef} style={{marginTop: "-17px"}}>+</div>
     </div>
   </div>
   )   
